bc9e9ca7cbb572b923c6164cadb3cac16490607e
elliott <39219466+GMorelli4@users.noreply.github.com>
2020-11-25 12:23:34 -0500

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,31 @@
+var audioCtx;
+
+var birdSource;
+
+var source;
+
+async function loadBuffer(bufferURL) {
+    //better to have a try/catch block here, but for simplicity...
+    const response = await fetch(bufferURL);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+  }
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    playBirdAudio();
+})
+
+async function playBirdAudio(){
+    audioCtx = new AudioContext()
+
+
+    birdSource = await loadBuffer('./silly_birds.mp3');
+    source = audioCtx.createBufferSource();
+    source.connect(globalGain).connect(audioCtx.destination);
+    source.buffer = birdSource;
+    source.start();
+   
+    source.connect(audioCtx.destination); 
+
+}
